a60c816a412b9c7a7d0a79f9e4387c1863df7cc8
girldiamond <epic-arol87@hotmail.com>
2024-10-27 20:39:32 -0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,50 @@
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+// Add your javascript here
+
+
+
+
+$(window).scroll(function(event) {
+  var scrollLeft = $(window).scrollLeft();
+  var scrollTop = $(window).scrollTop();
+ 
+  if(scrollTop >= 200){
+    document.getElementById('navbar').style.backgroundColor='#edeef9';
+  }
+
+ if(scrollTop < 100){
+    document.getElementById('navbar').style.backgroundColor='#f5f7fa';
+  }
+
+});
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
